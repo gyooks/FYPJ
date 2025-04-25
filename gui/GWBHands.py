2f9ab44c6c66ebcf77fe4456e5e2bc8ead0bf367
malcolmtgy <gymtuang001@mymail.sim.edu.sg>
2025-04-26 07:25:55 +0800

    made some changes to the filles

--- gui/GWBHands.py
+++ gui/GWBHands.py
@@ -11,32 +11,37 @@ root.title("GWBHands")
 root.geometry("1280x720")
 
 # Create a frame to hold the widgets
-center = ctk.CTkFrame(root, width=1600, height=900, fg_color="transparent")
-center.place(relx=0.5, rely=0.5, anchor="center")
+mainmenu = ctk.CTkFrame(root, width=1600, height=900, fg_color="transparent")
+mainmenu.place(relx=0.5, rely=0.5, anchor="mainmenu")
 
 #App title
 title = ctk.CTkLabel(root, text="GWBHands", font=("Segoe UI", 32, "bold"))
 title.pack(pady=20)
-title.place(relx=0.5, rely=0.1, anchor="center")
+title.place(relx=0.5, rely=0.1, anchor="mainmenu")
 
+#Logic for the buttons to open the various functions
+#def start_function():
+    # Placeholder function for start button
+    #print("Start function executed")
+    
 #Buttons
 button_font = ("Segoe UI", 16)
 button_width = 240
 button_height = 50
 
-btn_start = ctk.CTkButton(center, text="Start", font=button_font, width=button_width, height=button_height)
+btn_start = ctk.CTkButton(mainmenu, text="Start", font=button_font, width=button_width, height=button_height)
 btn_start.pack(pady=10)
 
-btn_preset = ctk.CTkButton(center, text="Preset Used: ", font=button_font, width=button_width, state="disabled")
+btn_preset = ctk.CTkButton(mainmenu, text="Preset Used: ", font=button_font, width=button_width, state="disabled")
 btn_preset.pack(pady=10)
 
-btn_changepreset = ctk.CTkButton(center, text="Change Preset", font=button_font, width=button_width, height=button_height)
+btn_changepreset = ctk.CTkButton(mainmenu, text="Change Preset", font=button_font, width=button_width, height=button_height)
 btn_changepreset.pack(pady=10)
 
-btn_settings = ctk.CTkButton(center, text="Settings", font=button_font, width=button_width, height=button_height)
+btn_settings = ctk.CTkButton(mainmenu, text="Settings", font=button_font, width=button_width, height=button_height)
 btn_settings.pack(pady=10)
 
-btn_exit = ctk.CTkButton(center, text="Exit", font=button_font, width=button_width, height=button_height, command=root.quit)
+btn_exit = ctk.CTkButton(mainmenu, text="Exit", font=button_font, width=button_width, height=button_height, command=root.quit)
 btn_exit.pack(pady=10)
 
 
