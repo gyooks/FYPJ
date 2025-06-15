5b5f915775baf73b98895c1019cb30d632873123
yipjunjie123 <yipjunjie123@gmail.com>
2025-06-16 02:36:58 +0800

    Setup a test folder that has ML and hand gestures recog with recording of new gestures

---
+++ gui/test/model/point_history_classifier/point_history_classifier.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+import tensorflow as tf
+import os
+
+class PointHistoryClassifier(object):
+    def __init__(
+        self,
+        model_path=None,
+        score_th=0.5,
+        invalid_value=0,
+        num_threads=1,
+    ):
+        # Use correct absolute path
+        if model_path is None:
+            script_dir = os.path.dirname(os.path.abspath(__file__))
+            model_path = os.path.join(script_dir, 'point_history_classifier.tflite')
+
+        # Check if model file exists
+        if not os.path.exists(model_path):
+            raise ValueError(f"Could not open model file. Path does not exist: {model_path}")
+
+        self.interpreter = tf.lite.Interpreter(model_path=model_path,
+                                               num_threads=num_threads)
+        self.interpreter.allocate_tensors()
+        self.input_details = self.interpreter.get_input_details()
+        self.output_details = self.interpreter.get_output_details()
+
+        self.score_th = score_th
+        self.invalid_value = invalid_value
+
+    def __call__(self, point_history):
+        input_tensor_index = self.input_details[0]['index']
+        self.interpreter.set_tensor(
+            input_tensor_index,
+            np.array([point_history], dtype=np.float32))
+        self.interpreter.invoke()
+
+        output_tensor_index = self.output_details[0]['index']
+        result = self.interpreter.get_tensor(output_tensor_index)
+
+        result_index = np.argmax(np.squeeze(result))
+
+        if np.squeeze(result)[result_index] < self.score_th:
+            return self.invalid_value
+
+        return result_index
